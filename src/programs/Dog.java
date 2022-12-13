506b288e6c65aedbeccc941607a94ec8ffc5b255
syed Hussain <syed.hussain@cerner.com>
2022-12-13 17:56:50 +0530

    2

---
+++ src/programs/Dog.java
@@ -0,0 +1,28 @@
+package programs;
+
+public class Dog
+{
+    private String name;
+
+    public Dog(String name)
+    {
+        this.name = name;
+    }
+
+    public boolean equals(Object other)
+    {
+        // ADD CODE HERE
+    }
+
+    public static void main(String[] args)
+    {
+        Dog d1 = new Dog("Rufus");
+        Dog d2 = new Dog("Sally");
+        Dog d3 = new Dog("Rufus");
+        Dog d4 = d3;
+        System.out.println(d1.equals(d2));
+        System.out.println(d2.equals(d3));
+        System.out.println(d1.equals(d3));
+        System.out.println(d3.equals(d4));
+    }
+}
