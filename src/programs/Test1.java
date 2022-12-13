e7b71e9961e01a3a00dd5cf54d43f779bbb7c7e1
syed Hussain <syed.hussain@cerner.com>
2022-12-13 17:55:26 +0530

    program1

---
+++ src/programs/Test1.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class Test1
+{
+    public static void talk()
+    {
+          System.out.println("hello there!");
+    }
+
+ 
+
+    public static void main(String[] args)
+    {
+          talk("Matthew");
+    }
+}
