934289498b50514eebbfe1f1cdbe15d2cb825812
Syed00711 <61380313+Syed00711@users.noreply.github.com>
2023-11-08 09:13:03 +0530

    Update MathClass.java

--- src/com/microcare/corejava/MathClass.java
+++ src/com/microcare/corejava/MathClass.java
@@ -3,15 +3,12 @@ package com.microcare.corejava;
 public class MathClass {
 
 	public static void main(String[] args) {
-		System.out.println(Math.max(5,6));
-		System.out.println(Math.min(7, 5));
-		
-		System.out.println(
-				
-				(int)(Math.random()*10)
-				
-				
-				);
+		System.out.println(Math.min(Basic.i, 6));
+	System.out.println(Math.max(-5.6, 6.5));
+	System.out.println(Math.floor(2.1));
+	
+	System.out.println(Math.floor(Math.sqrt(45465465)));
+	System.out.println(Math.cbrt(125));
 	
 
 	}
